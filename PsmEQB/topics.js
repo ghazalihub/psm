dff71b13d0a43844e93248434e45057c1483b980
ghazalihub <ghazalialshafi@gmail.com>
2025-09-26 12:04:25 +0530

    Add E

---
+++ PsmEQB/topics.js
@@ -0,0 +1,134 @@
+const topics = [
+    {
+        "id": 1,
+        "url": "Biomedical Waste Management",
+        "name": "Biomedical Waste Management",
+        "count": 26
+    },
+    {
+        "id": 2,
+        "url": "Biostatistics",
+        "name": "Biostatistics",
+        "count": 102
+    },
+    {
+        "id": 3,
+        "url": "Communicable Disease",
+        "name": "Communicable Disease",
+        "count": 114
+    },
+    {
+        "id": 4,
+        "url": "Concept of Health & Disease",
+        "name": "Concept of Health & Disease",
+        "count": 15
+    },
+    {
+        "id": 5,
+        "url": "Demography & Family Planning",
+        "name": "Demography & Family Planning",
+        "count": 68
+    },
+    {
+        "id": 6,
+        "url": "Disaster Management",
+        "name": "Disaster Management",
+        "count": 12
+    },
+    {
+        "id": 7,
+        "url": "Entomology",
+        "name": "Entomology",
+        "count": 26
+    },
+    {
+        "id": 8,
+        "url": "Environment & Health",
+        "name": "Environment & Health",
+        "count": 30
+    },
+    {
+        "id": 9,
+        "url": "Health Education & Communication",
+        "name": "Health Education & Communication",
+        "count": 9
+    },
+    {
+        "id": 10,
+        "url": "Health Indicators",
+        "name": "Health Indicators",
+        "count": 17
+    },
+    {
+        "id": 11,
+        "url": "Health Planning & Management",
+        "name": "Health Planning & Management",
+        "count": 14
+    },
+    {
+        "id": 12,
+        "url": "Health Programmes of Inida",
+        "name": "Health Programmes of Inida",
+        "count": 96
+    },
+    {
+        "id": 13,
+        "url": "Health System of India",
+        "name": "Health System of India",
+        "count": 49
+    },
+    {
+        "id": 14,
+        "url": "Infectious Disease Epidemiology",
+        "name": "Infectious Disease Epidemiology",
+        "count": 44
+    },
+    {
+        "id": 15,
+        "url": "Levels of Prevention",
+        "name": "Levels of Prevention",
+        "count": 8
+    },
+    {
+        "id": 16,
+        "url": "Maternal & Child Health",
+        "name": "Maternal & Child Health",
+        "count": 16
+    },
+    {
+        "id": 17,
+        "url": "Non Communicable Disease",
+        "name": "Non Communicable Disease",
+        "count": 23
+    },
+    {
+        "id": 18,
+        "url": "Nutrition",
+        "name": "Nutrition",
+        "count": 64
+    },
+    {
+        "id": 19,
+        "url": "Occupational Health",
+        "name": "Occupational Health",
+        "count": 27
+    },
+    {
+        "id": 20,
+        "url": "Pioneer’s In Public Health",
+        "name": "Pioneer’s In Public Health",
+        "count": 7
+    },
+    {
+        "id": 21,
+        "url": "Screening Test",
+        "name": "Screening Test",
+        "count": 49
+    },
+    {
+        "id": 22,
+        "url": "Vaccines",
+        "name": "Vaccines",
+        "count": 19
+    }
+];
