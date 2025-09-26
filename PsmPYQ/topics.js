ec37df4855e948461fb70ce27c269b832f260c66
ghazalihub <ghazalialshafi@gmail.com>
2025-09-26 12:07:44 +0530

    Add RR and Qs

---
+++ PsmPYQ/topics.js
@@ -0,0 +1,134 @@
+const topics = [
+    {
+        "id": 1,
+        "url": "PSM - AIIMS JUNE 2020",
+        "name": "PSM - AIIMS JUNE 2020",
+        "count": 16
+    },
+    {
+        "id": 2,
+        "url": "PSM - AIIMS MAY 2018",
+        "name": "PSM - AIIMS MAY 2018",
+        "count": 9
+    },
+    {
+        "id": 3,
+        "url": "PSM - AIIMS MAY 2019",
+        "name": "PSM - AIIMS MAY 2019",
+        "count": 14
+    },
+    {
+        "id": 4,
+        "url": "PSM - AIIMS NOV 2018",
+        "name": "PSM - AIIMS NOV 2018",
+        "count": 16
+    },
+    {
+        "id": 5,
+        "url": "PSM - AIIMS NOV 2019",
+        "name": "PSM - AIIMS NOV 2019",
+        "count": 24
+    },
+    {
+        "id": 6,
+        "url": "PSM - INI CET JULY 2021",
+        "name": "PSM - INI CET JULY 2021",
+        "count": 11
+    },
+    {
+        "id": 7,
+        "url": "PSM - INI CET MAY 2022",
+        "name": "PSM - INI CET MAY 2022",
+        "count": 17
+    },
+    {
+        "id": 8,
+        "url": "PSM - INI CET MAY 2023",
+        "name": "PSM - INI CET MAY 2023",
+        "count": 8
+    },
+    {
+        "id": 9,
+        "url": "PSM - INI CET NOV 2020",
+        "name": "PSM - INI CET NOV 2020",
+        "count": 8
+    },
+    {
+        "id": 10,
+        "url": "PSM - INI CET NOV 2021",
+        "name": "PSM - INI CET NOV 2021",
+        "count": 12
+    },
+    {
+        "id": 11,
+        "url": "PSM - INI CET NOV 2023",
+        "name": "PSM - INI CET NOV 2023",
+        "count": 7
+    },
+    {
+        "id": 12,
+        "url": "PSM - INI CET NOVEMBER 2022 (PART - A)",
+        "name": "PSM - INI CET NOVEMBER 2022 (PART - A)",
+        "count": 8
+    },
+    {
+        "id": 13,
+        "url": "PSM - INI CET NOVEMBER 2022 (PART - B)",
+        "name": "PSM - INI CET NOVEMBER 2022 (PART - B)",
+        "count": 14
+    },
+    {
+        "id": 14,
+        "url": "PSM - INICET MAY 2024",
+        "name": "PSM - INICET MAY 2024",
+        "count": 7
+    },
+    {
+        "id": 15,
+        "url": "PSM - NEET PG 2018",
+        "name": "PSM - NEET PG 2018",
+        "count": 20
+    },
+    {
+        "id": 16,
+        "url": "PSM - NEET PG 2019",
+        "name": "PSM - NEET PG 2019",
+        "count": 25
+    },
+    {
+        "id": 17,
+        "url": "PSM - NEET PG 2020",
+        "name": "PSM - NEET PG 2020",
+        "count": 18
+    },
+    {
+        "id": 18,
+        "url": "PSM - NEET PG 2021",
+        "name": "PSM - NEET PG 2021",
+        "count": 15
+    },
+    {
+        "id": 19,
+        "url": "PSM - NEET PG 2022",
+        "name": "PSM - NEET PG 2022",
+        "count": 12
+    },
+    {
+        "id": 20,
+        "url": "PSM - NEET PG 2023",
+        "name": "PSM - NEET PG 2023",
+        "count": 8
+    },
+    {
+        "id": 21,
+        "url": "PSM - NEET PG 2024",
+        "name": "PSM - NEET PG 2024",
+        "count": 33
+    },
+    {
+        "id": 22,
+        "url": "PSM-(INICET NOV 2024)",
+        "name": "PSM-(INICET NOV 2024)",
+        "count": 17
+    }
+];
